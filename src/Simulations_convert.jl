5413e3d17257960093a5e855b05b0fef44d8165d
Raphael <raphael.suda@gmx.net>
2020-09-21 08:51:18 +0200

    Fix convert function

--- src/Simulations_convert.jl
+++ src/Simulations_convert.jl
@@ -1,6 +1,6 @@
 function convert_model_data(df::DataFrame)
     for i in nrow(df)
-        sim = Simulation(0, df[i,:names], df[i,:alpha], df[i,:beta], (df[i,:eps_xx] df[i,:eps_zz], df[i,eps_xz]), true, (0.0,0.0,0.0), 0, (0.0,0.0,0.0), 0)#
+        sim = Simulation(0, df[i,:names], df[i,:alpha], df[i,:beta], (df[i,:eps_xx], df[i,:eps_zz], df[i,eps_xz]), true, (0.0,0.0,0.0), 0, (0.0,0.0,0.0), 0)
         write_model_data(sim)
     end
     return nothing
