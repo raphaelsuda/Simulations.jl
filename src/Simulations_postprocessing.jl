930b9315bcd15b45baf54755728a01f064dc453f
Raphael <raphael.suda@gmx.net>
2020-09-21 12:39:33 +0200

    Fix extract reaction forces

--- src/Simulations_postprocessing.jl
+++ src/Simulations_postprocessing.jl
@@ -72,8 +72,8 @@ function extract_reaction_forces(sim::Simulation)
     #TODO exception if simulation not ready yet
     # python files have to be executed in the simulation path
     simulation_path = joinpath("simulations", sim.name)
-    cd(simulation_path)
     reaction_forces_template(sim)
+    cd(simulation_path)
     run(`abq2019 cae noGUI=reaction_forces.py`)
     rm("reaction_forces.py")
     open("rf_done","w") do f
