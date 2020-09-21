d7ef27b321936f7489185815e62f305885e8bed3
Raphael <raphael.suda@gmx.net>
2020-09-21 17:35:42 +0200

    Fix reading reaction forces from report

--- src/Simulations_postprocessing.jl
+++ src/Simulations_postprocessing.jl
@@ -96,17 +96,16 @@ end
 function read_reaction_forces(sim::Simulation)
     #TODO exception if reaction forces not written yet
     simulation_path = joinpath("simulations", sim.name)
-    cd(simulation_path)
-    isdir("reaction_forces") ? nothing : mkdir("reaction_forces")
+    isdir(joinpath(simulation_path,"reaction_forces")) ? nothing : mkdir(joinpath(simulation_path,"reaction_forces"))
     @info "$(sim.name): Reading reaction forces from rpt"
     reaction_forces = Dict{String,Array{Number}}()
     for f in abaqus_report_files
-        data = readdlm(joinpath("abaqus_reports",f), skipstart=2)
+        data = readdlm(joinpath(simulation_path,"abaqus_reports",f), skipstart=2)
         time = data[:,1]
         rf = data[:,2]
         reaction_forces = Dict("time" => time, "$(f[1:end-4])" => rf)
     end
-    reaction_force_path = joinpath("reaction_forces","reaction_forces.dat")
+    reaction_force_path = joinpath(simulation_path,"reaction_forces","reaction_forces.dat")
     open(reaction_force_path,"w") do rff
         JSON.print(rff, reaction_forces)
     end
