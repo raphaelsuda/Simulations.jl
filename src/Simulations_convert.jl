e2a7e2b49636eb61314a0acd084480a717b53b26
Raphael <raphael.suda@gmx.net>
2020-09-21 08:48:30 +0200

    Auto stash before rebase of "master"

---
+++ src/Simulations_convert.jl
@@ -0,0 +1,13 @@
+function convert_model_data(df::DataFrame)
+    for i in nrow(df)
+        sim = Simulation(0, df[i,:names], df[i,:alpha], df[i,:beta], (df[i,:eps_xx] df[i,:eps_zz], df[i,eps_xz]), true, (0.0,0.0,0.0), 0, (0.0,0.0,0.0), 0)#
+        write_model_data(sim)
+    end
+    return nothing
+end
+
+function convert_model_data(path::AbstractString)
+    df = CSV.read(path)
+    convert_model_data(df)
+    return nothing
+end
