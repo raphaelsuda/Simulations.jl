92622ece4851817d81461f91bd621a3b6050ac77
Raphael <raphael.suda@gmx.net>
2020-09-21 12:44:38 +0200

    Create folder abaqus_reports if not existing

--- src/Simulations_postprocessing.jl
+++ src/Simulations_postprocessing.jl
@@ -34,7 +34,7 @@ function reaction_forces_template(sim::Simulation)
     "tmpName = rf11.name",
     "session.xyDataObjects.changeKey(tmpName, 'RF11')",
     "x0 = session.xyDataObjects['RF11']",
-    "session.writeXYReport(fileName='reaction_forces/RF11.rpt', appendMode=OFF, xyData=(x0, ",
+    "session.writeXYReport(fileName='abaqus_reports/RF11.rpt', appendMode=OFF, xyData=(x0, ",
     "    ))",
     "rf33 = sum((rf3_NWB, rf3_NWT))",
     "rf33.setValues(",
@@ -42,7 +42,7 @@ function reaction_forces_template(sim::Simulation)
     "tmpName = rf33.name",
     "session.xyDataObjects.changeKey(tmpName, 'RF33')",
     "x0 = session.xyDataObjects['RF33']",
-    "session.writeXYReport(fileName='reaction_forces/RF33.rpt', appendMode=OFF, xyData=(x0, ",
+    "session.writeXYReport(fileName='abaqus_reports/RF33.rpt', appendMode=OFF, xyData=(x0, ",
     "    ))",
     "rf13 = sum((rf3_SEB, rf3_SET))",
     "rf13.setValues(",
@@ -50,7 +50,7 @@ function reaction_forces_template(sim::Simulation)
     "tmpName = rf13.name",
     "session.xyDataObjects.changeKey(tmpName, 'RF13')",
     "x0 = session.xyDataObjects['RF13']",
-    "session.writeXYReport(fileName='reaction_forces/RF13.rpt', appendMode=OFF, xyData=(x0, ",
+    "session.writeXYReport(fileName='abaqus_reports/RF13.rpt', appendMode=OFF, xyData=(x0, ",
     "    ))",
     "rf31 = sum((rf1_NWB, rf1_NWT))",
     "rf31.setValues(",
@@ -58,7 +58,7 @@ function reaction_forces_template(sim::Simulation)
     "tmpName = rf31.name",
     "session.xyDataObjects.changeKey(tmpName, 'RF31')",
     "x0 = session.xyDataObjects['RF31']",
-    "session.writeXYReport(fileName='reaction_forces/RF31.rpt', appendMode=OFF, xyData=(x0, ",
+    "session.writeXYReport(fileName='abaqus_reports/RF31.rpt', appendMode=OFF, xyData=(x0, ",
     "    ))"]
     python_file_path = joinpath("simulations",sim.name,"reaction_forces.py")
     open(python_file_path,"w") do pf
@@ -70,6 +70,7 @@ end
 
 function extract_reaction_forces(sim::Simulation)
     #TODO exception if simulation not ready yet
+    isdir("abaqus_reports") ? nothing : mkdir("abaqus_reports")
     # python files have to be executed in the simulation path
     simulation_path = joinpath("simulations", sim.name)
     reaction_forces_template(sim)
