482cf6328b87f298132583d36c5314cd5dfd69b7
Raphael <raphael.suda@gmx.net>
2020-09-22 08:32:50 +0200

    Add text

--- src/Simulations_plotting.jl
+++ src/Simulations_plotting.jl
@@ -1,4 +1,4 @@
-function scatter_sampling(samp::Sampling; show_text=true, show_linear=true)
+function scatter_sampling(samp::Sampling; show_text=true, show_linear=true, title="")
     plot_simulations_data = CSV.read("plot_failure_data.dat")
     n_simulations = nrow(plot_simulations_data)
     p = plot(legend = :outertopleft)
@@ -8,12 +8,12 @@ function scatter_sampling(samp::Sampling; show_text=true, show_linear=true)
             line_zz = [plot_simulations_data[i,:sig_zz_lin] plot_simulations_data[i,:sig_zz_nonlin]]
             plot!(p, line_xx, line_zz, color=:gray, style=:dash, label=:none)
         end
-        scatter!(p, plot_simulations_data[!,:sig_xx_lin], plot_simulations_data[!,:sig_zz_lin], zcolor=plot_simulations_data[!,:sig_xz_lin], m = (:heat, :ltriangle))
+        scatter!(p, plot_simulations_data[!,:sig_xx_lin], plot_simulations_data[!,:sig_zz_lin], zcolor=plot_simulations_data[!,:sig_xz_lin], m = (:heat, :ltriangle), label="linear max")
     end
 	if show_text
-		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat))
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat), label="overall max", series_annotations = Plots.text.(plot_simulations_data["simulation"], 3, :gray, :topleft))
 	else
-		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat))
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat), label="overall max")
     end
     return p
 end
