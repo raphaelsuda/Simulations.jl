a8fa1bdce8a602a65b4e51b5869d45a8ed9b324f
Raphael <raphael.suda@gmx.net>
2020-09-22 07:57:58 +0200

    Change gradient and remove legend

--- src/Simulations_plotting.jl
+++ src/Simulations_plotting.jl
@@ -1,19 +1,19 @@
 function scatter_sampling(samp::Sampling; show_text=true, show_linear=true)
     plot_simulations_data = CSV.read("plot_failure_data.dat")
     n_simulations = nrow(plot_simulations_data)
-    p = plot()
+    p = plot(legend = :none)
     if show_linear
         for i in 1:n_simulations
             line_xx = [plot_simulations_data[i,:sig_xx_lin] plot_simulations_data[i,:sig_xx_nonlin]]
             line_zz = [plot_simulations_data[i,:sig_zz_lin] plot_simulations_data[i,:sig_zz_nonlin]]
             plot!(p, line_xx, line_zz, color=:gray, style=:dash)
         end
-        scatter!(p, plot_simulations_data[!,:sig_xx_lin], plot_simulations_data[!,:sig_zz_lin], zcolor=plot_simulations_data[!,:sig_xz_lin], markershape=:ltriangle)
+        scatter!(p, plot_simulations_data[!,:sig_xx_lin], plot_simulations_data[!,:sig_zz_lin], zcolor=plot_simulations_data[!,:sig_xz_lin], m = (:heat, :ltriangle))
     end
 	if show_text
-		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin])
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat))
 	else
-		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin])
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat))
     end
     return p
 end
