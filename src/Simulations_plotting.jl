37f48b80eb9309454cfeff366c4335388ebd1fcd
Raphael <raphael.suda@gmx.net>
2020-09-22 12:57:43 +0200

    Fix

--- src/Simulations_plotting.jl
+++ src/Simulations_plotting.jl
@@ -18,9 +18,28 @@ function scatter_sampling(samp::Sampling; show_text=true, show_linear=true, titl
     return p
 end
 
+function scatter_sampling!(p::Plots.Plot,samp::Sampling; show_text=true, show_linear=true, title="", xlims=:none, ylims=:none) 
+    plot_simulations_data = CSV.read("plot_failure_data.dat")
+    n_simulations = nrow(plot_simulations_data)
+    if show_linear
+        for i in 1:n_simulations
+            line_xx = [plot_simulations_data[i,:sig_xx_lin], plot_simulations_data[i,:sig_xx_nonlin]]
+            line_zz = [plot_simulations_data[i,:sig_zz_lin], plot_simulations_data[i,:sig_zz_nonlin]]
+            plot!(p, line_xx, line_zz, color=:gray, style=:dash, label=:none)
+        end
+        scatter!(p, plot_simulations_data[!,:sig_xx_lin], plot_simulations_data[!,:sig_zz_lin], zcolor=plot_simulations_data[!,:sig_xz_lin], m = (:heat, :ltriangle), label="linear max")
+    end
+	if show_text
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat), label="overall max", xlims=xlims, ylims=ylims, series_annotations = Plots.text.(plot_simulations_data["simulation"], 3, :gray, :topleft))
+	else
+		scatter!(p, plot_simulations_data[!,:sig_xx_nonlin], plot_simulations_data[!,:sig_zz_nonlin], zcolor=plot_simulations_data[!,:sig_xz_nonlin], m = (:heat), label="overall max", xlims=xlims, ylims=ylims)
+    end
+    return p
+end
+
 function plot_history(samp::Sampling; title="")
     simulations = filter_simulations(samp, true)
-    p = plot(legend=:none, title=title)
+    p = plot(title=title)
     for s in values(simulations)
         simulation_path = joinpath("simulations",s.name)
         stresses = JSON.parsefile(joinpath(simulation_path,"stresses","stresses.dat"))
