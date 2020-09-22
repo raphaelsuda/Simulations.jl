7bf61c2f8cce9b89c38dc30462853b23555e7966
Raphael <raphael.suda@gmx.net>
2020-09-22 08:53:24 +0200

    Add plot limits

--- src/Simulations_plotting.jl
+++ src/Simulations_plotting.jl
@@ -1,7 +1,7 @@
 function scatter_sampling(samp::Sampling; show_text=true, show_linear=true, title="", xlims=:none, ylims=:none) 
     plot_simulations_data = CSV.read("plot_failure_data.dat")
     n_simulations = nrow(plot_simulations_data)
-    p = plot(legend = :outertopleft, title=title)
+    p = plot(legend = :outertopleft, title=title, xlims=xlims, ylims=ylims)
     if show_linear
         for i in 1:n_simulations
             line_xx = [plot_simulations_data[i,:sig_xx_lin] plot_simulations_data[i,:sig_xx_nonlin]]
