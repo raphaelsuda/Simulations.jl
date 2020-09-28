34ac11a0362642ae0ec7519eee81199b7ca6f3ec
Raphael <raphael.suda@gmx.net>
2020-09-28 09:00:14 +0200

    Sort simulations before saving failure data

--- src/Simulations.jl
+++ src/Simulations.jl
@@ -316,6 +316,7 @@ function collect_failure_data(samp::Sampling)
                      :sig_xx_nonlin => sig_xx_nonlin,
                      :sig_zz_nonlin => sig_zz_nonlin,
                      :sig_xz_nonlin => sig_xz_nonlin)
+    sort!(plot_df, :simulation)
     CSV.write("plot_failure_data.dat", plot_df)
     return plot_df
 end
