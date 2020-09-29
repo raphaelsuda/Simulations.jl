a6f1112dece1cf987098f5a1c5e4dc8f583ee88c
Raphael <raphael.suda@gmx.net>
2020-09-29 13:58:37 +0200

    Fixie

--- src/Simulations_convert.jl
+++ src/Simulations_convert.jl
@@ -1,7 +1,8 @@
 function convert_model_data(df::DataFrame)
     for i in 1:nrow(df)
         @info "Converting model data for simulation $(df[i,:names])"
-        sim = Simulation(0, df[i,:names], df[i,:alpha], df[i,:beta], (df[i,:eps_xx], df[i,:eps_zz], df[i,:eps_xz]), true, (0.0,0.0,0.0), 0, (0.0,0.0,0.0), 0)
+        ID = parse(Int64, split(df[i,:names],'-')[end])
+        sim = Simulation(0, df[i,:names], ID, df[i,:alpha], df[i,:beta], (df[i,:eps_xx], df[i,:eps_zz], df[i,:eps_xz]), true, (0.0,0.0,0.0), 0, (0.0,0.0,0.0), 0)
         write_model_data(sim)
     end
     return nothing
